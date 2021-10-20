86c4d31414ff19b199aae56da1ca00515f973ccc
Valerie Heaton <vheaton2015@gmail.com>
2021-10-20 00:15:47 -0400

    random UUID start, JSON edits

--- InternshipDriver.java
+++ InternshipDriver.java
@@ -3,9 +3,24 @@
  * 
  * @author Siri, Maddie, Valerie, Ali
  */
+
+//import random UUID tool
+import java.util.UUID;
+
 public class InternshipDriver {
     
     public static void main(String [] args) {
+        
+        /**
+         * UUID generator section
+         */
+        UUID uuid = UUID.randomUUID();
+        String uuidString = uuid.toString();
+
+        System.out.print(uuidString);
+        /**
+         * End UUID generator section
+         */
 
     }
 
--- InternshipList.java
+++ InternshipList.java
@@ -5,7 +5,7 @@ public class InternshipList {
      * Variables for the InternshipList class
      */
     private InternshipList internshipList;
-    private ArrayList<Internship> internships;
+    private ArrayList<Internships> internships;
 
     public String job;
     /**
@@ -15,13 +15,16 @@ public class InternshipList {
     /**
      * The public getInstance method fetches the instance of 'Internship'
      */
-    public Internship getInstance() {}
+    public Internships getInstance() {
+        return null;
+    }
     /**
      * The public array list method containing the 'internship' instance that fetches internship entries
      * named 'jobs.'
      */
-    public ArrayList<Internship> getInternships(String job) {
+    public ArrayList<Internships> getInternships(String job) {
         this.job = job;
+        return internships;
     }
     
 }
