5c42025ddeef08c1bfa6e819aede658752b2d40d
siriavula <sirigavula@gmail.com>
2021-10-27 00:49:40 -0400

    changes

---
+++ StudentApplication.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class StudentApplication {
+    
+    private InternshipList internshipList;
+    private UserList userList;
+    private User user;
+
+    public StudentApplication() {}
+
+    public ArrayList<Internships> findInternships() {
+    
+    }
+
+    public ArrayList<Internships> findInternships(String keyword) {
+
+    }
+
+    public User createAccount(String firstName, String lastName, String userName) { // will maybe need to add more to parameters
+
+    }
+
+    public User login(String userName, String password) {
+
+    }
+
+    public ArrayList<Internships> getMyInternships() {
+
+    }
+
+}
