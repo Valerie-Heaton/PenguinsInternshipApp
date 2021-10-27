909bbde8d1763a9c29d5fe96c16a466bb6da1b77
maddie havraniak <havranim@email.sc.edu>
2021-10-27 09:17:29 -0400

    making changes to UI

--- InternshipUI.java
+++ InternshipUI.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class InternshipUI {
     private static final String WELCOME_MESSAGE = "Welcome to the inernship app";
-    private String[] mainMenuOptions = {"Filter,", "Search"};
+    private String[] mainMenuOptions = {"Login,", "Prints out resume to a text file", "filter", "Login with a job listing"};
     private Scanner scanner; 
     private Internships internship;
     
@@ -10,6 +10,7 @@ public class InternshipUI {
         internship = new Internships(null, null, null);
     }
     public void run() {
+<<<<<<< HEAD
         System.out.println(WELCOME_MESSAGE);
     //
     /**while(true) {
@@ -26,8 +27,53 @@ public class InternshipUI {
 		//}
 		//System.out.println("\n");
 	//}
+=======
+		System.out.println(WELCOME_MESSAGE);
+		
+		//Loop as long as we want to keep interacting with the library
+		while(true) {
+			displayMainMenu();
+			int userCommand = getUserCommand(mainMenuOptions.length);
+			
+			if(userCommand == -1) {
+				System.out.println("Not a valid command");
+				continue;
+			}
+			
+			//if they picked the last option then log them out
+			if(userCommand == mainMenuOptions.length -1) break;
+		
+			switch(userCommand) {
+				case(0):
+                    login();//TODO find login method in class 
+                    //TODO put in instructions in scenario 2 in dropbox
+					break;
+				case(1):
+					printsOutResume();//TODO find prints out resume method
+					break;
+				case(2):
+					Filter();//find filter method
+					break;
+				case(3):
+					LoginWithanExistingJob();//TODO find method with existing jjob
+					break;
+			}
+		}
+		
+		System.out.println("Good bye, and have a nice day");
+		
+	}
+        
+    private void displayMainMenu() {
+		System.out.println("\n************ Main Menu *************");
+		for(int i=0; i< mainMenuOptions.length; i++) {
+			System.out.println((i+1) + ". " + mainMenuOptions[i]);
+		}
+		System.out.println("\n");
+    }
+}
+>>>>>>> bffa61e (making changes to git)
 	
     
 
-    }
-}
+
