3108ad53bb15f1c53b75ba15fad3d457b584d734
Tehranie <alijt@email.sc.edu>
2021-10-29 10:27:21 -0700

    studentapp modified

---
+++ StudentApplication.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class StudentApplication {
+    /**
+     * Variables for the StudentApplication class
+     */
+    private InternshipList internshipList;
+    private UserList userList;
+    private User user;
+
+    public StudentApplication() {}
+    public ArrayList<Internships> findInternships() {}
+    public ArrayList<Internships> findInternships(String keyword) {}
+    public User createAccount(String firstName, String lastName, String userName, ...) {}
+    public User login(String userName, String password) {}
+    public ArrayList<Internships> getMyInternships() {}
+    public reviewInternship(User rater, int rating, String comment) {}
+
+
+}
