0e012f733eabb5677e845e356ff57c0d5ccd4e1d
maddie havraniak <havranim@email.sc.edu>
2021-11-01 17:46:04 -0400

    making changes

--- InternshipUI.java
+++ InternshipUI.java
@@ -5,27 +5,20 @@ public class InternshipUI {
     private Scanner scanner; 
    private static Internships internship;
    private static Resume resume;
+   private static StudentApplication studentApplication;
    private Student student;
 
     
     InternshipUI() {
         scanner = new Scanner(System.in);
-<<<<<<< HEAD
-        //internship = new Internships(null, null, null, false, false);
-=======
-		//Internships internship = new Internships(title);
-		
->>>>>>> b99d3726a0c37dd2f4d8c74afffb1d241d844250
-    }
+	}
     public void run() {
         System.out.println(WELCOME_MESSAGE);
-	
-		
+
 		//Loop as long as we want to keep interacting with the library
 		while(true) {
 			displayMainMenu();
 			int userCommand = getUserCommand(mainMenuOptions.length);
-			
 			
 			if(userCommand == -1) {
 				System.out.println("Not a valid command");
@@ -34,11 +27,11 @@ public class InternshipUI {
 			//if they picked the last option then log them out
 			if(userCommand == mainMenuOptions.length -1) break;
 		
+		
 			switch(userCommand) {
 				case(0):
-			
 					login(userName, password);
-					Student.insertResume();
+					student.insertResume();
 					internship.getSkillsRequired();
 					resume.getEducation();//not a method yet
 					resume.getExperience();
@@ -48,17 +41,17 @@ public class InternshipUI {
 					break;
 				case(2):
 					internship.getSkillsRequired();
-					Internships(internship);
+					studentApplication.findInternships(keyword);
 					//how do i find jobs that require java knowledge
 					//TODO go back to internship class and find the internships 
 					//TODO we have to make a method for picking and selecting a job
 					break;
 				case(3):
-					LoginWithanExistingJob();
-
+					login(userName, password);//login as an employer
+					studentApplication.reviewInternships(rater, rating, comment);
+					student.getStudentUsers();
 					//TODO find method with existing job
 					//TODO once we make the method for selecting and applying to a job we allow the listing to be views
-
 					break;
 			}
 		}
@@ -71,7 +64,7 @@ public class InternshipUI {
 		}
 		System.out.println("\n");
 	}
-}
+
 
 
 private int getUserCommand(int numCommands) {
@@ -94,5 +87,6 @@ private int getUserCommand(int numCommands) {
 	//}
 	
 		}
+	}
 
 
