cf692c55cd8f925dc26c0762164ec416774dd905
siriavula <sirigavula@gmail.com>
2021-11-01 03:17:45 -0400

    changes

--- DataConstants.java
+++ DataConstants.java
@@ -15,4 +15,7 @@ public class DataConstants {
     protected static final String USER_DESCRIPTION = "description";
     protected static final String USER_RATING = "rating";
     protected static final String USER_SKILLS_REQUIRED = "skills required";
+    protected static final String USER_LOCATION = "location";
+    protected static final String USER_IN_PERSON = "in person";
+    protected static final String USER_REMOTE = "remote";
 }
