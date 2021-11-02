3f8d9dc58a39aadc6d80696b3c9330f6db405a86
Valerie Heaton <vheaton2015@gmail.com>
2021-11-02 15:11:21 -0400

    minor changes

--- DataConstants.java
+++ DataConstants.java
@@ -11,13 +11,13 @@ public class DataConstants {
     protected static final String USER_EXPERIENCE = "experience";
     protected static final String USER_SKILLS = "skills";
     protected static final String USER_EDUCATION = "education";
-    protected static final String USER_INTERNSHIPS_OFFERED = "internships offered";
+    protected static final String USER_INTERNSHIPS_OFFERED = "internshipsOffered";
     protected static final String USER_TITLE = "title";
     protected static final String USER_DESCRIPTION = "description";
     protected static final String USER_RATING = "rating";
-    protected static final String USER_SKILLS_REQUIRED = "skills required";
+    protected static final String USER_SKILLS_REQUIRED = "skillsRequired";
     protected static final String USER_LOCATION = "location";
-    protected static final String USER_IN_PERSON = "in person";
+    protected static final String USER_IN_PERSON = "inPerson";
     protected static final String USER_REMOTE = "remote";
     protected static final String USER_COMPANY = "company";
 
