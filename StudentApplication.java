5bb5da2787b01950c1525064c2774cdbc7c0f786
Valerie Heaton <vheaton2015@gmail.com>
2021-11-01 23:41:47 -0400

    student application

--- StudentApplication.java
+++ StudentApplication.java
@@ -1,22 +1,11 @@
-/**
- * The Student Application class holds all of the data needed for the student to input
- * regarding a specific internship. The data will include the students login, password,
- * as well as finding internships, and rating them as well as providing comments.
- * This is the main interface.
- */
-
 import java.util.*;
 
 public class StudentApplication {
-    /**
-     * Private variables for the StudentApplication class
-     */    
+
     private InternshipList internshipList;
     private UserList userList;
     private User user;
-    /**
-     * Create instances
-     */
+
     public StudentApplication() {
         this.internshipList = internshipList; // should it be get instance
         this.userList = userList;
@@ -28,59 +17,20 @@ public class StudentApplication {
     }
 
     public ArrayList<Internships> findInternships(String keyword) {
-        this.keyword = keyword;
-        if(internshipList.contains(keyword))
-        return internshipList;
+        return internshipList.getInternships(keyword);
     }
-    
-    public boolean createAccount(String userName, String firstName, String lastName, String email) { 
+
+    public boolean createAccount(String userName, String firstName, String lastName, String email) {
         return userList.addUser(userName, firstName, lastName, email); // will maybe need to add more to parameters
     }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 72a81caa51f51c1e9bd751b1f116b72ad339e406
-    public User createAccount(String firstName, String lastName, String userName) { 
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.userName = userName;
-<<<<<<< HEAD
-        return user;
-        // will maybe need to add more to parameters
-    // public boolean createEmployerAccount(String userName, String firstName, String lastName, String email) { 
-    //     return userList.addEmployerUser(userName, firstName, lastName, email); // will maybe need to add more to parameters
-}
-
-    public boolean login(String userName) { // was not originally boolean
-=======
-        return user;// will maybe need to add more to parameters
-
-    // public boolean createEmployerAccount(String userName, String firstName, String lastName, String email) { 
-    //     return userList.addEmployerUser(userName, firstName, lastName, email); // will maybe need to add more to parameters
-    // }
-
-
-    public boolean loginUser(String userName) { // was not originally boolean
-
     public boolean login(String userName, String password) { // was not originally boolean
-
->>>>>>> 72a81caa51f51c1e9bd751b1f116b72ad339e406
-        if(!userList.haveUser(userName)) {
+        if (!userList.haveUser(userName)) {
             return false;
         }
         user = userList.getUser(userName, password);
         return true;
     }
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 72a81caa51f51c1e9bd751b1f116b72ad339e406
-
-    // public ArrayList<Internships> getMyInternships() {
-    //     return internshipList;
-    // }
 
     public void logout() {
         userList.saveUsers();
