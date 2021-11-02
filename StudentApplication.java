791e8032bc1045f70ff8e9d955f5dded479a1d5b
Tehranie <alijt@email.sc.edu>
2021-11-01 17:34:16 -0700

    updated student app

--- StudentApplication.java
+++ StudentApplication.java
@@ -1,11 +1,22 @@
+/**
+ * The Student Application class holds all of the data needed for the student to input
+ * regarding a specific internship. The data will include the students login, password,
+ * as well as finding internships, and rating them as well as providing comments.
+ * This is the main interface.
+ */
+
 import java.util.*;
 
 public class StudentApplication {
-    
+    /**
+     * Private variables for the StudentApplication class
+     */    
     private InternshipList internshipList;
     private UserList userList;
     private User user;
-
+    /**
+     * Create instances
+     */
     public StudentApplication() {
         this.internshipList = internshipList;
         this.userList = userList;
@@ -17,11 +28,15 @@ public class StudentApplication {
     }
 
     public ArrayList<Internships> findInternships(String keyword) {
+        this.keyword = keyword;
         if(internshipList.contains(keyword))
         return internshipList;
     }
 
     public User createAccount(String firstName, String lastName, String userName) { 
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
         return user;// will maybe need to add more to parameters
 
     }
@@ -36,6 +51,9 @@ public class StudentApplication {
     }
 
     public reviewInternship(User rater, int rating, String comment) {
+        this.firstName = rater;
+        this.lastName = rating;
+        this.userName = comment;
         return user;
         return rating;
         return comment;
