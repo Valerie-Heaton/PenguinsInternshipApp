5912b4fa5259969ea8ed56fc04a842db983aaf79
Tehranie <alijt@email.sc.edu>
2021-11-02 21:36:22 -0700

    FINALIZED rating comments

--- Rating.java
+++ Rating.java
@@ -10,7 +10,10 @@ public class Rating {
    private String comment;
    
    /**
-    * this method will return the rating of the user 
+    * This is a constructor method for the Rating class that will return the rating of the user.
+    * @param rater is the appropriate User and is assigned to this.rater.
+    * @param rating is the appropriate Int and is assigned to this.rating.
+    * @param comment is the appropriate String and is assigned to this.comment.
     */
    public Rating(User rater, int rating, String comment) {
       this.rater = rater;
@@ -18,19 +21,25 @@ public class Rating {
       this.comment = comment;
    }
 
-   /**
-   *this method will get the rater
+  /**
+   * This method fetches the rater.
+   * @return returns the rater variable.
    */
    public User getRater() {
       return this.rater;
    }
-   
+
+  /**
+   * This method fetches the rating.
+   * @return returns the rating variable.
+   */
    public int getRating() {
       return this.rating;
    }
 
-   /**
-   * this method will get the comment of the user
+  /**
+   * This method fetches the comment of the user.
+   * @return returns the comment variable.
    */
    public String getComment() {
       return this.comment;
